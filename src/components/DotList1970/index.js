086b78f5abd79c0ab5ac6a7ffa3e0043fad0443f
Isaias Lucena <isaiiaslucena@gmail.com>
2021-03-11 01:33:52 -0300

    Ajustado conteudo da tag a em todos os componentes DotList

--- src/components/DotList1970/index.js
+++ src/components/DotList1970/index.js
@@ -4,13 +4,13 @@ import "../../pages/Home/styles.css";
 const DotList1970 = (props) => {
   return (
     <>
-      <a href="#first-screen" className="dot" style={{ marginTop: "3px", marginLeft: "-6px" }} />
+      <a href="#first-screen" className="dot" style={{ marginTop: "3px", marginLeft: "-6px" }} > </a>
       <span className="dot" style={{ marginTop: "35px", marginLeft: "-11px",  backgroundColor: props.backgroundColor }} />
-      <a href="#third-screen" className="dot" style={{ marginTop: "68px", marginLeft: "-11px" }} />
-      <a href="#fourth-screen" className="dot" style={{ marginTop: "101px", marginLeft: "-11px" }} />
-      <a href="#fifth-screen" className="dot" style={{ marginTop: "135px", marginLeft: "-11px" }} />
-      <a href="#sixth-screen" className="dot" style={{ marginTop: "168px", marginLeft: "-11px" }} />
-      <a href="#seventh-screen" className="dot" style={{ marginTop: "200px", marginLeft: "-11px" }} />
+      <a href="#third-screen" className="dot" style={{ marginTop: "68px", marginLeft: "-11px" }} > </a>
+      <a href="#fourth-screen" className="dot" style={{ marginTop: "101px", marginLeft: "-11px" }} > </a>
+      <a href="#fifth-screen" className="dot" style={{ marginTop: "135px", marginLeft: "-11px" }} > </a>
+      <a href="#sixth-screen" className="dot" style={{ marginTop: "168px", marginLeft: "-11px" }} > </a>
+      <a href="#seventh-screen" className="dot" style={{ marginTop: "200px", marginLeft: "-11px" }} > </a>
     </>
   );
 };
--- src/components/DotList1990/index.js
+++ src/components/DotList1990/index.js
@@ -4,13 +4,13 @@ import "../../pages/Home/styles.css";
 const DotList1990 = (props) => {
   return (
     <>
-      <a href="#first-screen" className="dot" style={{ marginTop: "0px", marginLeft: "-2px" }} />
-      <a href="#second-screen" className="dot" style={{ marginTop: "35px", marginLeft: "-11px" }} />
+      <a href="#first-screen" className="dot" style={{ marginTop: "0px", marginLeft: "-2px" }} > </a>
+      <a href="#second-screen" className="dot" style={{ marginTop: "35px", marginLeft: "-11px" }} > </a>
       <span className="dot" style={{ marginTop: "101px", marginLeft: "-11px",  backgroundColor: props.backgroundColor }} />
-      <a href="#third-screen" className="dot" style={{ marginTop: "68px", marginLeft: "-11px" }} />
-      <a href="#fifth-screen" className="dot" style={{ marginTop: "135px", marginLeft: "-11px" }} />
-      <a href="#sixth-screen" className="dot" style={{ marginTop: "168px", marginLeft: "-11px" }} />
-      <a href="#seventh-screen" className="dot" style={{ marginTop: "200px", marginLeft: "-11px" }} />
+      <a href="#third-screen" className="dot" style={{ marginTop: "68px", marginLeft: "-11px" }} > </a>
+      <a href="#fifth-screen" className="dot" style={{ marginTop: "135px", marginLeft: "-11px" }} > </a>
+      <a href="#sixth-screen" className="dot" style={{ marginTop: "168px", marginLeft: "-11px" }} > </a>
+      <a href="#seventh-screen" className="dot" style={{ marginTop: "200px", marginLeft: "-11px" }} > </a>
     </>
   );
 };
